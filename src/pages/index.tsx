2ec816bc75531abba2f6767be65092e35e06e4d8
Mark <cyrus70@hurest.com>
2023-09-17 15:53:38 +0900

    배포 스크립트 내 이름 수정 및 테스트용 페이지 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -140,9 +140,7 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
       <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <span style={headingAccentStyles}>— Mark's musings! 🎉🎉🎉</span>
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
